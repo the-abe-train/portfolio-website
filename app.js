a5904268b0d37d080329f5248933ad28ba1dfab7
Abe Train <abraham.train@gmail.com>
2021-11-27 15:56:20 -0500

    Added send POST req

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'routes')));
+
+
+// View engine setup
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
+})
+
+app.post('/send', (req, res) => {
+  const output = `
+  <p>You have a new contact request</p>
+  <h3>Contact Details</h3>
+  <ul>
+    <li>Name: ${req.body.name}</li>
+    <li>Email: ${req.body.email}</li>
+  </ul>
+  <h3>Message</h3>
+  <p>${req.body.message}</p>
+  `
+  "use strict";
 })
 
 app.listen(port, () => {
