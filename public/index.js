8e2b4b27d58263df0672188dcf5be58959441fb2
Abe Train <abraham.train@gmail.com>
2021-11-29 23:03:40 -0500

    Scroll and responsiveness upgrades

--- public/index.js
+++ public/index.js
@@ -1,16 +1,28 @@
-const contactBtn = document.querySelector('#contact');
-const overlay = document.querySelector('#Overlay');
-const navList = document.querySelector('#nav-list');
 const hamburger = document.querySelector('#hamburger');
-const body = document.querySelector('body');
-const forms = document.querySelectorAll('.form-container');
+const navList = document.querySelector('#nav-list');
+const body = document.body;
+const scrollUpBtn = document.querySelector('#scroll-up-btn');
 
+const contactBtn = document.querySelector('#contact-btn');
+const contactFrom = document.querySelector('#contact-form-container');
+const subscribeBtn = document.querySelector('#subscribe-btn');
+const subForm = document.querySelector('#subscribe-form-container');
+const thankYou = document.querySelector('#thank-you');
+const overlay = document.querySelector('#Overlay');
 
-function toggleOverlay(e) {
-  if (e.target.closest('.container') === null) {
-    overlay.classList.toggle('hidden');
-    forms.forEach(form => form.classList.remove('hidden'));
+function appearOnScroll() {
+  if (
+    (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20)
+    && window.innerWidth >= 600) {
+    scrollUpBtn.style.display = "block";
+  } else {
+    scrollUpBtn.style.display = "none";
   }
+}
+
+function scrollToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
 
 function toggleHamburger(e) {
@@ -20,19 +32,40 @@ function toggleHamburger(e) {
 
 function closeHamburger(e) {
   if (e.target.closest('nav') === null) {
-    if (navList.classList.contains('hamburger-open')) {
-      navList.classList.toggle('hamburger-closed');
-      navList.classList.toggle('hamburger-open');
-    }
+    navList.classList.add('hamburger-closed');
+    navList.classList.remove('hamburger-open');
+  }
+}
+
+function closeOverlay(e) {
+  if (e.target.closest('form') === null) {
+    overlay.classList.toggle('hidden');
+    contactFrom.classList.add('hidden');
+    subForm.classList.add('hidden');
   }
 }
 
 function closeForm(e) {
   e.target.closest('.form-container').classList.toggle('hidden');
+  thankYou.classList.remove('hidden');
 }
 
-contactBtn.addEventListener('click', toggleOverlay);
-overlay.addEventListener('click', toggleOverlay);
+function openContactForm() {
+  overlay.classList.toggle('hidden');
+  contactFrom.classList.remove('hidden');
+}
+
+function openSubForm() {
+  overlay.classList.toggle('hidden');
+  subForm.classList.remove('hidden');
+}
+
+window.onscroll = function () { appearOnScroll() };
+scrollUpBtn.addEventListener('click', scrollToTop);
 hamburger.addEventListener('click', toggleHamburger);
 body.addEventListener('click', closeHamburger);
-forms.forEach(form => form.addEventListener('submit', closeForm));
+
+contactBtn.addEventListener('click', openContactForm);
+contactFrom.addEventListener('submit', closeForm);
+subscribeBtn.addEventListener('click', openSubForm);
+overlay.addEventListener('click', closeOverlay);
